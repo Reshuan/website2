acb348fe3a655e0a15f2c8864acd15b811dbc2f9
Reshuan <reshuanw@gmail.com>
2025-03-19 16:51:18 +0000

    Wed, Mar 19, 2025, 9:51 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,1 +1,8 @@
-
+const screens = document.querySelectiorAll('.screen')
+const start_btn = document.getElementById('start-btn')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+const game_container = document.getElementById
+('game-container)
+const timeEl = document.getElementbyId('time')
+const scoreEl = document.getElementbyId('score')
+const message = document.getElementbyId('message')
